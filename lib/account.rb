6c72cd535c2570f0049d94ba4c7171da8a00d9a1
ollie beney <ombeney@gmail.com>
2020-11-02 11:49:12 +0000

    corrected rubocop formatting offences

--- lib/account.rb
+++ lib/account.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
+# account class to complete bank tech test
 class Account
   attr_reader :balance
 
-STATEMENT_HEADER = "date || credit || debit || balance"
+  STATEMENT_HEADER = 'date || credit || debit || balance'
 
   def initialize
     @balance = 0
   end
-
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,14 +1,14 @@
+# frozen_string_literal: true
+
 require 'account'
 describe Account do
-  let(:account){ Account.new }
+  let(:account) { Account.new }
 
-  it "has a statement header constant" do
-    expect(Account::STATEMENT_HEADER).to eq "date || credit || debit || balance"
+  it 'has a statement header constant' do
+    expect(Account::STATEMENT_HEADER).to eq 'date || credit || debit || balance'
   end
 
-  it "initializes with a balance of 0" do
+  it 'initializes with a balance of 0' do
     expect(account.balance).to eq 0
   end
-
-
 end
